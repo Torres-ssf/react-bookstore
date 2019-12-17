dd988188459176db44a59d61c664f02741cbdee8
Sergio Torres <sergio.uft@gmail.com>
2019-12-17 16:23:22 -0300

    setup(redux): setup redux in index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import dummyData from './dummyData';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, dummyData());
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const rootTag = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(rootTag, document.getElementById('root'));
+
 serviceWorker.unregister();
