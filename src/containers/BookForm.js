4e6c5b7e7fff6ef91833e58b1fcb12c9271396cf
Sergio Torres <sergio.uft@gmail.com>
2019-12-17 17:09:36 -0300

    chore(fix): Fix stickler errors

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,26 +1,31 @@
 import React from 'react';
 
-class BookForm extends React.Component {
-  constructor(props) {
+class BookForm extends React.Component { // eslint-disable-line
+  constructor(props) { // eslint-disable-line
     super(props);
   }
 
   render() {
     const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi', 'Drama'];
-    const options = categories.map((e, i) => <option key={`${e}-${i}`} value={e}>{e}</option>);
+    const options = categories.map((e, i) => <option key={`${e}-`} value={e}>{e}</option>);
 
     return (
       <div>
         <h2>Add New Book</h2>
         <form className="form">
-          <label>Name: </label>
-          <input type="text" placeholder="book name" required />
-          <label>Author: </label>
-          <input type="text" placeholder="book author" required />
-          <label>Category: </label>
-          <select name="dropdown">
-            {options}
-          </select>
+          <label>Name:
+            <input type="text" placeholder="book name" required />
+          </label>
+          <label>
+            Author:
+            <input type="text" placeholder="book author" required />
+          </label>
+          <label>
+            Category:
+            <select name="dropdown">
+              {options}
+            </select>
+          </label>
           <button type="submit">Add New Book</button>
         </form>
       </div>
