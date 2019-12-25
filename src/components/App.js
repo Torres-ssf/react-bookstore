7928599981b66c08518dc078bd274bbd9eb08878
Sergio Torres <sergio.uft@gmail.com>
2019-12-25 12:11:23 -0300

    style(app): re style the whole application

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,32 @@ import BookList from '../containers/BookList';
 import BookFormControl from '../containers/BookFormControl';
 import './App.scss';
 
-function App() {
-  return (
-    <main className="App">
-      <Header />
-      <BookList />
-      <BookFormControl />
-    </main>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFilter: false,
+    };
+
+    this.setShowFilterValue = this.setShowFilterValue.bind(this);
+  }
+
+  setShowFilterValue(bool) {
+    this.setState({
+      showFilter: bool,
+    });
+  };
+
+  render() {
+    const { showFilter } = this.state;
+    return (
+      <main className="App">
+        <Header setFilterDisplay={this.setShowFilterValue} />
+        <BookList showFilter ={showFilter}/>
+        <BookFormControl />
+      </main>
+    );
+  }
 }
 
 export default App;
