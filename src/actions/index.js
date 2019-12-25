b32a12ed0dd3ed6f6cbdd063732683018aaf6ee1
Sergio Torres <sergio.uft@gmail.com>
2019-12-25 11:56:34 -0300

    refactor(progress): Deleted progress reducer and add progress functionality to book reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const UPDATE_BOOK_PROGRESS = 'UPDATE_BOOK_PROGRESS';
 export const CHANGE_FILTER = 'CHANGE_FILTER';
-export const UPDATE_PROGRESS = 'UPDATE_PROGRESS';
 
 export const createBookMsg = book => ({ type: CREATE_BOOK, book });
 
 export const removeBookMsg = id => ({ type: REMOVE_BOOK, id });
 
-export const changeBookFilter = category => ({ type: CHANGE_FILTER, category });
+export const updateBookProgress = (index, progress) => ({ type: UPDATE_BOOK_PROGRESS, index, progress });
 
-export const updateProgress = page => ({ type: UPDATE_PROGRESS, page });
+export const changeBookFilter = category => ({ type: CHANGE_FILTER, category });
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,4 +1,4 @@
-import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
+import { CREATE_BOOK, REMOVE_BOOK, UPDATE_BOOK_PROGRESS } from '../actions/index';
 
 const bookReducer = (state = [], action) => {
   switch (action.type) {
@@ -10,6 +10,17 @@ const bookReducer = (state = [], action) => {
 
     case REMOVE_BOOK:
       return state.filter(book => book.id !== action.id);
+
+    case UPDATE_BOOK_PROGRESS:
+      const { index, progress } = action;
+      return [
+        ...state.slice(0, index),
+        {
+          ...state[index],
+          progress,
+        },
+        ...state.slice(index + 1, state.length),
+      ];
 
     default:
       return state;
