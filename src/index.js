dc04679c49dd4e264e65845b65289a6a75dbf8ab
Sergio Torres <sergio.uft@gmail.com>
2020-01-09 12:20:48 -0300

    refactor(index.js): Modify index.js to use the new create middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
-import { saveToLocalStorage, loadFromLocalStorage } from './utility/localStorage';
+import booksMiddleware from './middlewares/booksMiddleware';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, loadFromLocalStorage());
-store.subscribe(() => saveToLocalStorage(store.getState()));
+const store = createStore(rootReducer, applyMiddleware(booksMiddleware));
 
 const rootTag = (
   <Provider store={store}>
