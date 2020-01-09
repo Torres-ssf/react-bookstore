b66201349e5c9fd77b7f6716e0fd133e1af1c372
Sergio Torres <sergio.uft@gmail.com>
2020-01-09 12:15:43 -0300

    feature(actions): Add new actions to fetch data and add book data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,21 @@
-export const CREATE_BOOK = 'CREATE_BOOK';
-export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const FETCH_BOOK_DATA = 'FETCH_BOOK_DATA'
+export const ADD_BOOK_DATA = 'ADD_BOOK_DATA';
+export const ADD_NEW_BOOK = 'ADD_NEW_BOOK';
+export const DELETE_BOOK = 'DELETE_BOOK';
 export const UPDATE_BOOK_PROGRESS = 'UPDATE_BOOK_PROGRESS';
 export const CHANGE_FILTER = 'CHANGE_FILTER';
 
-export const createBookMsg = book => ({ type: CREATE_BOOK, book });
+export const fetchBookData = () => ({ type: FETCH_BOOK_DATA });
 
-export const removeBookMsg = id => ({ type: REMOVE_BOOK, id });
+export const addBookData = data => ({ type: ADD_BOOK_DATA, data });
 
-export const updateBookProgress = (index, progress) => ({
+export const addNewBook = book => ({ type: ADD_NEW_BOOK, book });
+
+export const deleteBook = id => ({ type: DELETE_BOOK, id });
+
+export const updateBookProgress = (id, index, progress) => ({
   type: UPDATE_BOOK_PROGRESS,
+  id,
   index,
   progress,
 });
