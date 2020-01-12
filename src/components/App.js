69b79567dda3a3ff4a2c0b6158a99eae5257b3d7
Sergio Torres <sergio.uft@gmail.com>
2020-01-12 11:14:17 -0300

    Refactor(App): Refactor App component to use react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import BookList from '../containers/BookList';
-import BookFormControl from '../containers/BookFormControl';
 import './App.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showFilter: false,
-    };
-    this.setShowFilterValue = this.setShowFilterValue.bind(this);
-  }
-
-  setShowFilterValue(bool) {
-    this.setState({
-      showFilter: bool,
-    });
   }
 
   render() {
-    const { showFilter } = this.state;
     return (
-      <main className="App">
-        <Header setFilterDisplay={this.setShowFilterValue} />
-        <BookList showFilter={showFilter} />
-        <BookFormControl />
-      </main>
+      <Router>
+        <main className="App">
+          <Header setFilterDisplay={this.setShowFilterValue} />
+          <Switch>
+            <Route path="/" component={BookList} />
+          </Switch>
+        </main>
+      </Router>
     );
   }
 }
