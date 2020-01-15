650ab47f6cadf1c4e1f88326bafa149f3fb18d90
Sergio Torres <sergio.uft@gmail.com>
2020-01-15 14:46:39 -0300

    feature(Categories): WIP create Categories component to add/remove/update book categories

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import BookList from '../containers/BookList';
+import Categories from '../containers/Categories';
 import './App.scss';
+import EditBook from '../containers/EditBook';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Router>
-        <main className="App">
-          <Header setFilterDisplay={this.setShowFilterValue} />
-          <Switch>
-            <Route path="/" component={BookList} />
-          </Switch>
-        </main>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <main className="App">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={BookList} />
+        <Route path="/edit-book" render={(props) => <EditBook {...props} />} />
+        <Route path="/categories" render={(props) => <Categories {...props} title={props.title} />} />
+      </Switch>
+    </main>
+  </Router>
+);
 
 export default App;
