2e5812fa4b350c63dc8891026fa24fb1399685d0
Sergio Torres <sergio.uft@gmail.com>
2020-01-15 14:35:43 -0300

    feature(action update): Create update action for book data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const FETCH_BOOK_DATA = 'FETCH_BOOK_DATA'
 export const ADD_BOOK_DATA = 'ADD_BOOK_DATA';
 export const ADD_NEW_BOOK = 'ADD_NEW_BOOK';
+export const UPDATE_BOOK = 'UPDATE_BOOK';
 export const DELETE_BOOK = 'DELETE_BOOK';
 export const UPDATE_BOOK_PROGRESS = 'UPDATE_BOOK_PROGRESS';
 export const CHANGE_FILTER = 'CHANGE_FILTER';
@@ -10,6 +11,13 @@ export const fetchBookData = () => ({ type: FETCH_BOOK_DATA });
 export const addBookData = data => ({ type: ADD_BOOK_DATA, data });
 
 export const addNewBook = book => ({ type: ADD_NEW_BOOK, book });
+
+export const updateBook = (id, index, data) => ({
+  type: UPDATE_BOOK,
+  id,
+  index,
+  data,
+});
 
 export const deleteBook = id => ({ type: DELETE_BOOK, id });
 
